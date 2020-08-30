908a152a8ba1d59655dd14f7beefa905b8a8c764
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-08-30 20:56:14 -0300

    Criando componente Header

---
+++ src/components/HcodeHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-expand">
+        <a href class="link navbar-brand mr-auto">
+          <img
+            src="./../assets/logo.svg"
+            alt="Hcode Treinamentos"
+            class="img d-inline-block alignt-top"
+            id="logo"
+          />
+          Futebol Clube
+        </a>
+
+        <div class="links mr-2">
+          <a href="#" class="btn mr-5">Inicio</a>
+          <a href="#" class="btn mr-5">Times</a>
+          <a href="#" class="btn mr-5">Classificação</a>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  background-color: #000;
+}
+#logo {
+  width: 150px;
+  height: 100%;
+}
+.links {
+  display: inline-flex;
+}
+.link {
+  color: #d48e23;
+  font-size: 30px;
+  font-family: 'Rajdhani';
+}
+.links a {
+  color: #fff;
+}
+.links a:focus {
+  outline: none !important;
+  box-shadow: none !important;
+}
+</style>
