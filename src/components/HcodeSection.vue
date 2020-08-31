721a6647c91afee4fd6d610ee72e7bfb31e5d853
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-08-30 21:43:50 -0300

    Adicionando banner

---
+++ src/components/HcodeSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HcodeSectionBanner />
+  </div>
+</template>
+
+<script>
+import HcodeSectionBanner from './HcodeSectionBanner';
+export default {
+  components: {
+    HcodeSectionBanner,
+  },
+};
+</script>
+
+<style scoped></style>
